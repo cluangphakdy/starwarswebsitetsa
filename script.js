cbaecbaa90c89bf21ed26a3e182d6f30d7c1ec61
cluangphakdy <CoolColin110@gmail.com>
2025-01-15 17:51:28 -0500

    Update script.js
    added more notes

--- script.js
+++ script.js
@@ -9,4 +9,5 @@ toggleInstructions.addEventListener('click', function() {
  * -Three.js loaded in index.html file to add 3d objects in a website -1/15/25
  * -Three.js accepts .gtlf, .glb, .fbx, or .onj file types
  * -Making lightsaber parts in blender as of 1/15/25
+ * -.gltF is the preferred and best file type for Three.js, so it is recommended to get models in that file type
  */
