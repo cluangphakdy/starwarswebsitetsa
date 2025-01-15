90276f56d7bff291c75417cb10209ea70d2bfaee
cluangphakdy <CoolColin110@gmail.com>
2025-01-15 17:47:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ const text = document.getElementById('text');
 
 toggleInstructions.addEventListener('click', function() {
     text.classList.toggle("hidden");
-});
+}); 
+
+/**
+ * -Three.js loaded in index.html file to add 3d objects in a website -1/15/25
+ * -Three.js accepts .gtlf, .glb, .fbx, or .onj file types
+ * -Making lightsaber parts in blender as of 1/15/25
+ */
